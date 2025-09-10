42d680698ca6d260e16fbf680874c57702e3ec61
kariya-fumihiko27 <fumihiko_kariya@icloud.com>
2025-09-10 22:30:25 +0900

    CORS許可設定の追加

---
+++ src/main/java/com/example/presentation/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.presentation.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	// CORSを許可するホストを定義する
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
