d6d9633c74f3077e9e91b5bda15d3790d17d0471
kariya-fumihiko27 <fumihiko_kariya@icloud.com>
2025-09-17 21:06:58 +0900

    存在しないリソースへのアクセス時の例外処理を追加

--- src/main/java/com/example/presentation/advice/ControllerAspect.java
+++ src/main/java/com/example/presentation/advice/ControllerAspect.java
@@ -3,11 +3,13 @@ package com.example.presentation.advice;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,7 +23,7 @@ public class ControllerAspect {
 	/**
 	 * 入力値の検証に失敗した場合に検証エラー内容を返却する
 	 */
-	@ExceptionHandler(value = {MethodArgumentNotValidException.class})
+	@ExceptionHandler(value = MethodArgumentNotValidException.class)
 	public ResponseEntity<Map<String, String>> handleValidationError(MethodArgumentNotValidException e){
 		log.error(e.getMessage());
 		
@@ -31,9 +33,22 @@ public class ControllerAspect {
 	}
 	
 	/**
+	 * ハンドリングされないパスのリクエストを受信した場合にNot Foundを返却する
+	 */
+	@ExceptionHandler(value = NoResourceFoundException.class)
+	public ResponseEntity<Map<String, String>> noResourceFoundError(NoResourceFoundException e){
+		log.error(e.getMessage());
+		
+		Map<String, String> cause = new HashMap<>();
+		cause.put("method", e.getHttpMethod().name());
+		cause.put("path", e.getResourcePath());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(cause);
+	}
+	
+	/**
 	 * 補足されない例外が発生した場合、ステータスコード500でエラーを返却する
 	 */
-	@ExceptionHandler(value = {Exception.class})
+	@ExceptionHandler(value = Exception.class)
 	public ResponseEntity<UnhandledError> handleUncatchedException(Exception e){
 		log.error(e.getMessage());
 		
