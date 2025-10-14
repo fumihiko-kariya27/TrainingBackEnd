bf894f8df4093bed613ac05eaf70d03c3152ee32
kariya-fumihiko27 <fumihiko_kariya@icloud.com>
2025-10-14 21:52:54 +0900

    Repository層のテストコード追加及びバグフィックス対応

--- src/main/java/com/example/presentation/training/UserController.java
+++ src/main/java/com/example/presentation/training/UserController.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domain.training.account.TrainingUser;
 import com.example.domain.training.code.EJoinFlag;
 import com.example.service.user.UserService;
 
-@Controller
+@RestController
 public class UserController {
 	
 	@Autowired
