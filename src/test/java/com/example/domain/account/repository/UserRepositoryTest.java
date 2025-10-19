502b8917fadaa74786f2fb1d9e7abcd253556223
kariya-fumihiko27 <fumihiko_kariya@icloud.com>
2025-10-19 10:23:40 +0900

    [append] UserRepositoryTest

---
+++ src/test/java/com/example/domain/account/repository/UserRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.example.domain.account.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+
+import com.example.domain.training.account.TrainingUser;
+import com.example.domain.training.account.repository.UserRepository;
+import com.example.domain.training.code.EJoinFlag;
+import com.example.test.config.CustomRepositoryTest;
+
+@CustomRepositoryTest
+@Sql("UserRepositoryTest.sql")
+public class UserRepositoryTest {
+	
+	@Autowired
+	UserRepository repository;
+	
+	private static List<TrainingUser> users = List.of(
+		new TrainingUser(1, "kariya", LocalDate.of(2024, 4, 10), "0", "SLVR", "1"),
+		new TrainingUser(2, "shinoda", LocalDate.of(2024, 5, 21), "0", "BRNZ", "1"),
+		new TrainingUser(3, "anada", LocalDate.of(2024, 6, 9), "1", "BRNZ", "0"),
+		new TrainingUser(4, "iwata", LocalDate.of(2024, 10, 18), "0", "GOLD", "1"),
+		new TrainingUser(5, "takahashi", LocalDate.of(2024, 8, 23), "0", "SLVR", "0"),
+		new TrainingUser(6, "ookita", LocalDate.of(2024, 11, 19), "1", "GOLD", "1")
+	);
+	
+	@Test
+	@DisplayName("ユーザー一覧を取得する")
+	public void getUsers() {
+		
+		List<TrainingUser> actual = repository.selectUsers();
+		
+		assertThat(actual).containsExactlyInAnyOrderElementsOf(users);
+	}
+	
+	@Test
+	@DisplayName("入会フラグが有効であるユーザー一覧を取得する")
+	public void getActiveUser(){
+		List<TrainingUser> actual = repository.selectUsersJoinFlagEqual(EJoinFlag.ACTIVE);
+		
+		List<TrainingUser> expActiveUsers = users.stream().filter(user -> user.isActive()).toList();
+		assertThat(actual).containsExactlyInAnyOrderElementsOf(expActiveUsers);
+	}
+}
--- src/test/java/com/example/presentation/training/TrainingControllerTest.java
+++ src/test/java/com/example/presentation/training/TrainingControllerTest.java
@@ -3,12 +3,13 @@ package com.example.presentation.training;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -17,7 +18,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.domain.training.account.TrainingUser;
+import com.example.domain.training.course.EAttendance;
 import com.example.domain.training.course.ProgramCourse;
+import com.example.domain.training.history.TrainingHistory;
 import com.example.service.training.TrainingService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -39,7 +43,7 @@ public class TrainingControllerTest {
 		new ProgramCourse("AWSCL", "AWS Cloud Basics", "AWSクラウドの基礎研修です", LocalDate.of(2025, 2, 18), LocalDate.of(2025, 3, 10))
 	);
 	
-	@BeforeAll
+	@BeforeEach
 	public void init() {
 		when(service.getTrainingCourse()).thenReturn(trainings);
 	}
@@ -47,18 +51,72 @@ public class TrainingControllerTest {
 	@Test
 	@DisplayName("トレーニング情報をJSON形式で取得する")
 	public void getTrainingAsJson() throws Exception {
-		String expJson = objectMapper.writeValueAsString(trainings);
-		
 		String actualJson = mockMvc.perform(
 			get("/training")
 			.accept(MediaType.APPLICATION_JSON)
 		)
-		.andExpect(status().isOk())
-		.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+		.andExpectAll(
+			status().isOk(),
+			content().contentType(MediaType.APPLICATION_JSON)
+		)
+		.andDo(print())
 		.andReturn()
 		.getResponse()
 		.getContentAsString();
 		
+		String expJson = objectMapper.writeValueAsString(trainings);
+		assertThat(actualJson).isEqualTo(expJson);
+	}
+	
+	@Test
+	@DisplayName("トレーニング情報をXML形式で取得する")
+	public void getTrainingAsXml() throws Exception {
+		mockMvc.perform(
+			get("/training")
+			.accept(MediaType.APPLICATION_XML)
+		)
+		.andExpectAll(
+			status().isOk(),
+			content().contentTypeCompatibleWith(MediaType.APPLICATION_XML)
+		)
+		.andDo(print())
+		.andReturn()
+		.getResponse()
+		.getContentAsString();
+	}
+	
+	@Test
+	@DisplayName("トレーニング受講履歴を取得する")
+	public void getTrainingHistories() throws Exception {
+		TrainingUser expUser = new TrainingUser(1, "kariya", LocalDate.of(2024, 4, 10), "0", "SLVR", "1");
+		TrainingHistory history1 = new TrainingHistory();
+		history1.setUser(expUser);
+		history1.setAttendance(EAttendance.COMPLETED);
+		history1.setCourse(new ProgramCourse("AGILE", "Agile Development", "アジャイル開発の基礎研修です", LocalDate.of(2025, 4, 5), LocalDate.of(2025, 4, 15)));
+		
+		TrainingHistory history2 = new TrainingHistory();
+		history2.setUser(expUser);
+		history2.setAttendance(EAttendance.IN_PROCESS);
+		history2.setCourse(new ProgramCourse("AWSCL", "AWS Cloud Basics", "AWSクラウドの基礎研修です", LocalDate.of(2025, 2, 18), LocalDate.of(2025, 3, 10)));
+		
+		List<TrainingHistory> histories = List.of(history1, history2);
+		when(service.getHistories(anyString())).thenReturn(histories);
+		
+		String actualJson = mockMvc.perform(
+			get("/training/history")
+			.param("userId", "dummy")
+			.accept(MediaType.APPLICATION_JSON)
+		)
+		.andExpectAll(
+			status().isOk(),
+			content().contentType(MediaType.APPLICATION_JSON)
+		)
+		.andDo(print())
+		.andReturn()
+		.getResponse()
+		.getContentAsString();
+		
+		String expJson = objectMapper.writeValueAsString(histories);
 		assertThat(actualJson).isEqualTo(expJson);
 	}
 }
