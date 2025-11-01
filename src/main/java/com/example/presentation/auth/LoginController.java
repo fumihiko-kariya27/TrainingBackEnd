71fbb8702a1504bdd1672e3693f8299d9826f8fd
kariya-fumihiko27 <fumihiko_kariya@icloud.com>
2025-11-01 21:42:19 +0900

    JWT認証不正時の例外処理を追加

--- src/main/java/com/example/presentation/auth/LoginController.java
+++ src/main/java/com/example/presentation/auth/LoginController.java
@@ -1,10 +1,15 @@
 package com.example.presentation.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,15 +29,19 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+    	
+    	try {
+    		Authentication authentication = authenticationManager.authenticate(
+    	        new UsernamePasswordAuthenticationToken(
+    	            loginRequest.getUserId(),
+    	            loginRequest.getPassword()
+    	        )
+    	    );
 
-        Authentication authentication = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                loginRequest.getUserId(),
-                loginRequest.getPassword()
-            )
-        );
-
-        String jwt = tokenProvider.generateToken(authentication);
-        return ResponseEntity.ok(Map.of("accessToken", jwt));
+    	    String jwt = tokenProvider.generateToken(authentication);
+    	    return ResponseEntity.ok(Map.of("accessToken", jwt));
+    	} catch(BadCredentialsException | UsernameNotFoundException | DisabledException | LockedException e) {
+    		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Invalid credentials"));
+    	}
     }
 }
